b935e0d7e441b4a3a33929fbabaa84d603b91cff
Khala Wright <khalawright5@gmail.com>
2023-04-21 22:21:07 -0400

    updated layout

--- js/site.js
+++ js/site.js
@@ -1,43 +1,62 @@
+function getValues(){
 //inputs
-const loanAmount = document.getElementById('loanAmount');
-const term = document.getElementById('term');
-const interestRate = document.getElementById('interestRate');
-const form = document.getElementById('mortgage');
+const loanAmount = document.getElementById('loanAmount').value;
+const term = document.getElementById('term').value;
+const interestRate = document.getElementById('interestRate').value;
+const form = document.getElementById('mortgage').value;
+
+}
 
 
 //calculate monthly payment
-function calculateMonthlyPayment(loanAmount, term, interestRate) {
+function calculate(loanAmount, term, interestRate) {
 let mortgage = loanAmount.value * (interestRate/1200) / (1 - Math.pow(1 + (interestRate/1200), -term));
 
-console.log(mortgage)
 return parseFloat(mortgage.toFixed(2));
+
+var result="";
+
+result += `<table>`;
+result += `<tr><td>Total Principle:</td>`;
+result += `<td class="text-end"> ${loanAmount} </td></tr>`;
+
+result += `<tr><td>Total Interest:</td>`;
+result += `<td class="text-end"> ${interestRate} </td></tr>`;
+
+result += `<tr><td>Term in Months:</td>`;
+result += `<td class="text-end"> ${term} </td></tr>`;
+
+result += `</table>`;
+
+document.getElementById('loan_info').innerHTML = result;
+
 }
 
-//link submit button to calculatemonthlypayment function
-form.onsubmit = (e) => {
-    e.preventDefault();
-    validate();
 
-    let monthlyPayment = calculateMonthlyPayment(loanAmount, term.value, interestRate.value);
+    
 
+
+
+    // display monthly payment
+    let monthlyPayment = calculate(loanAmount, term.value, interestRate.value);
     document.getElementById('monthlyPayment').innerHTML = `$ ${monthlyPayment}`;
-}
 
-function validate(){
-    if(
-        loanAmount.value ==="" || term.value ==="" || interestRate.value ===""
-    ){
+
+
+
+
+
+// function validate(){
+//     if(
+//         loanAmount.value ==="" || term.value ==="" || interestRate.value ===""
+//     ){
          
- let alert = Swal.fire({
-    icon: 'error',
-    title: 'Oops...',
-    text: 'All fields must be completed.',
-  });
-    }
+//  let alert = Swal.fire({
+//     icon: 'error',
+//     title: 'Oops...',
+//     text: 'All fields must be completed.',
+//   });
+//     }
 
-}
+// }
 
-
-function remainingBalance() {
-
-}
